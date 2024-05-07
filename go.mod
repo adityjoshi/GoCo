6455b0fec4ac0b9dc9db213a1624e95225747ad2
Aditya Joshi <aditya03joshi@gmail.com>
2024-05-07 23:29:37 +0530

    bencoding

---
+++ go.mod
@@ -0,0 +1,5 @@
+module PARSER
+
+go 1.22.2
+
+require github.com/adityjoshi/GoCo v0.0.0-20240507172609-e1b8534093b9
--- main.go
+++ main.go
@@ -5,20 +5,17 @@ import (
 	"fmt"
 	"os"
 
-	"./bencoding"
+	"github.com/adityjoshi/GoCo/bencoding"
 )
 
 func main() {
-	// Create a bufio.Reader from os.Stdin
 	reader := bufio.NewReader(os.Stdin)
 
-	// Call the Decode function from the bencoding package
 	result, err := bencoding.Decode(reader)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
-	// Print the result
 	fmt.Println("Result:", result)
 }
